594bff411084801a8a9e12234ffcdcbaa3fac5e2
takakatu <takakatu0201@gmail.com>
2020-02-14 13:10:24 +0900

    (サーバーサイド)商品詳細ページ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,11 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :seller, class_name: "User"
   validates :name,:description,:status,:shipping_charges,:postage,:shopping_date,:exhibiting,:category_id, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :size
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shopping_date
+  belongs_to_active_hash :postage
   
 end
