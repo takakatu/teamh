324a9d5b7f41824f8fce4b89b099fa655e74c85b
ShimadaRikuya <59195586+ShimadaRikuya@users.noreply.github.com>
2020-02-14 18:13:04 +0900

    item_id-解決

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,15 @@ Rails.application.routes.draw do
       get 'get_category_children', defaults: {format: 'json'}
       get 'get_category_grandchildren', defaults: {format: 'json'}
     end
-    resources :purchase, only: [:show] do
-      collection do
-        post 'pay', to: 'purchase#pay'
-        get 'done', to: 'purchase#done'
-      end
+  end
+
+  resources :purchase, only: [:show] do
+    collection do
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
     end
   end
+  
   resources :images, only: [:new, :create]
 
   resources :creditcards, only: [:new, :show] do
