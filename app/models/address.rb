b38714d7eea47e934331250c8be011fd5a60c87b
tomio1030 <59253596+tomio1030@users.noreply.github.com>
2020-02-17 18:39:48 +0900

    修正ばり

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :d_first_name,:d_last_name,:d_first_name_kana,:d_last_name_kana,:zip,:state,:city,:street,:building,presence: true
-  
+  validates :d_first_name,:d_last_name,:d_first_name_kana,:d_last_name_kana,:zip,:state,:city,:street,presence: true
+  validates :zip , length: { is: 7 }
 end
